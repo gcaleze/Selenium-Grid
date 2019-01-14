efa8be080b157dfba4c7c051618ebfd5ef5ba14a
gcaleze <gcaleze@svi.com.ph>
2019-01-14 15:29:24 +0800

    1st commit

---
+++ SeleniumGrid/src/test/java/example/NewTest.java
@@ -0,0 +1,32 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+	String driverPath = "C:\\Selenium\\chromedriver.exe";
+	public WebDriver driver;
+
+	@Test
+	public void test1() {
+		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("qwerty");
+		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys(Keys.RETURN);
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
