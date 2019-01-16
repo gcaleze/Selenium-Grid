fa12acf678ba56bc961b6832afabb3c1a25fdf29
gcaleze <gcaleze@svi.com.ph>
2019-01-16 14:22:47 +0800

    Update NewTest.java

--- SeleniumGrid/src/test/java/example/NewTest.java
+++ SeleniumGrid/src/test/java/example/NewTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 
 public class NewTest {
-	String driverPath = "/usr/bin/chromedriver";
+	String driverPath;
 	//String driverPath = "/usr/bin/geckodriver";
 	public WebDriver driver;
 
@@ -21,10 +21,23 @@ public class NewTest {
 
 	@BeforeClass
 	public void beforeClass() {
+		locateDriver();
 		//System.setProperty("webdriver.gecko.driver", driverPath);
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+	}
+	
+	public void locateDriver() {
+		String OS = System.getProperty("os.name");
+		System.out.println("locateDriver()");
+		if (OS.equals("Windows 7")) {
+			driverPath = "C:\\Selenium\\chromedriver.exe";
+			System.out.println("Windows 7");
+		} else {
+			System.out.println("Other OS");
+			driverPath = "/usr/bin/chromedriver";
+		}
 	}
 
 	@AfterClass
