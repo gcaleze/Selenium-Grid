9f265dea1e515bed754fd7ad005d015f3e989ef3
gcaleze <gcaleze@svi.com.ph>
2019-01-18 18:32:04 +0800

    Update NewTest.java

--- SeleniumGrid/src/test/java/example/NewTest.java
+++ SeleniumGrid/src/test/java/example/NewTest.java
@@ -1,47 +1,24 @@
 package example;
+
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
- 
-
-
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Test;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-
+import org.testng.annotations.Test;
 
 public class NewTest {
-    //protected ThreadLocal<RemoteWebDriver> threadDriver = null;
 
-	  
-	public static void main(String[] args) throws MalformedURLException {
-		
-		 WebDriver driver;
-		 
-
-  
-DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-  capabilities.setCapability(FirefoxDriver.BINARY,new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe").getAbsolutePath());
-
- driver = new RemoteWebDriver(new URL("http://192.168.125.97:4444/wd/hub"), capabilities);
- 
- 
-  
-   driver.get("http://google.com");
-   System.out.println(driver.getTitle());
- 
-  
-  
-
-     }
+	@Test(priority = 1)
+	public void test1() throws MalformedURLException {
+		WebDriver driver;
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability(FirefoxDriver.BINARY,
+				new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe").getAbsolutePath());
+		driver = new RemoteWebDriver(new URL("http://192.168.125.97:4444/wd/hub"), capabilities);
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+	}
 }
-
