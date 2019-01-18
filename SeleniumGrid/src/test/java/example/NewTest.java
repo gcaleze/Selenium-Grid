51efce4f7392b5936f080da7eeda3e96876538e7
gcaleze <gcaleze@svi.com.ph>
2019-01-18 17:55:39 +0800

    Update NewTest.java

--- SeleniumGrid/src/test/java/example/NewTest.java
+++ SeleniumGrid/src/test/java/example/NewTest.java
@@ -1,47 +1,47 @@
 package example;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+ 
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
 
 public class NewTest {
-	String driverPath;
-	//String driverPath = "/usr/bin/geckodriver";
-	public WebDriver driver;
+    //protected ThreadLocal<RemoteWebDriver> threadDriver = null;
 
-	@Test
-	public void test1() {
-		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("Test test");
-		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys(Keys.RETURN);
-	}
+	  
+	public static void main(String[] args) throws MalformedURLException {
+		
+		 WebDriver driver;
+		 
 
-	@BeforeClass
-	public void beforeClass() {
-		locateDriver();
-		//System.setProperty("webdriver.gecko.driver", driverPath);
-		System.setProperty("webdriver.chrome.driver", driverPath);
-		driver = new ChromeDriver();
-		driver.get("https://www.google.com/");
-	}
-	
-	public void locateDriver() {
-		String OS = System.getProperty("os.name");
-		System.out.println("locateDriver()");
-		if (OS.equals("Windows 7")) {
-			driverPath = "C:\\Selenium\\chromedriver.exe";
-			System.out.println("Windows 7");
-		} else {
-			System.out.println("Other OS");
-			driverPath = "/usr/bin/chromedriver";
-		}
-	}
+  
+DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+  capabilities.setCapability(FirefoxDriver.BINARY,new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe").getAbsolutePath());
 
-	@AfterClass
-	public void afterClass() {
-	}
+ driver = new RemoteWebDriver(new URL("http://192.168.125.97:4444/wd/hub"), capabilities);
+ 
+ 
+  
+   driver.get("http://google.com");
+   System.out.println(driver.getTitle());
+ 
+  
+  
 
+     }
 }
+
