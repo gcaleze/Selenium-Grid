01968b584a5d3e24e00b022eac3f7ac6e27f36b1
gcaleze <gcaleze@svi.com.ph>
2019-01-18 20:34:56 +0800

    Update NewTest.java

--- SeleniumGrid/src/test/java/example/NewTest.java
+++ SeleniumGrid/src/test/java/example/NewTest.java
@@ -6,6 +6,7 @@ import java.net.URL;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -15,22 +16,28 @@ public class NewTest {
 	WebDriver driver;
     String baseURL, nodeURL;
 	@Test(priority = 1)
-	public void test1() throws MalformedURLException {
-//		WebDriver driver;
-//		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-//		capabilities.setCapability(FirefoxDriver.BINARY,
-//				new File("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe").getAbsolutePath());
-//		driver = new RemoteWebDriver(new URL("http://192.168.125.97:4444/wd/hub"), capabilities);
-//		driver.get("http://google.com");
-//		System.out.println(driver.getTitle());
-        baseURL = "http://demo.guru99.com/test/guru99home/";
-        nodeURL = "http://192.168.43.223:4444/wd/hub";
-        DesiredCapabilities capability = DesiredCapabilities.chrome();
-        String label = System.getProperty("SELENIUM_LABEL");
-        //capability.setCapability("jenkins.label", Platform.WINDOWS);
-        capability.setCapability("jenkins.label", label);
-        //capability.setBrowserName("chrome");
-        //capability.setPlatform(Platform.WINDOWS);
-        driver = new RemoteWebDriver(new URL(nodeURL), capability);
+	public void test1(){		
+		
+//-----------------------------------------------		
+//        baseURL = "http://demo.guru99.com/test/guru99home/";
+//        nodeURL = "http://192.168.43.223:4444/wd/hub";
+//        DesiredCapabilities capability = DesiredCapabilities.chrome();
+//        String label = System.getProperty("SELENIUM_LABEL");
+//        //capability.setCapability("jenkins.label", Platform.WINDOWS);------------
+//        capability.setCapability("jenkins.label", label);
+//        //capability.setBrowserName("chrome");-----------
+//        //capability.setPlatform(Platform.WINDOWS);------
+//        driver = new RemoteWebDriver(new URL(nodeURL), capability);
+//------------------------------
+        
+        
+		System.setProperty("webdriver.gecko.driver","/usr/bin/geckodriver");		 
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+	
+//		System.setProperty("webdriver.chrome.driver", "/usr/bin/geckodriver");
+//		driver =new ChromeDriver();
+//		driver.get("http://www.google.com");
+	
 	}
 }
